0522a207fad15941ed7023d1b6347fb5fe8e61ec
Dan Pickett <dan.pickett@launchware.com>
2013-06-14 09:53:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smurf::Application.config.secret_token = 'fb2dff912853769c11061282c99b759d5fe02fec3db6fc20c8c02a280fd5a76e39fb5b5c7248e0e39dbc5cb4a63824468a5292570fb0825d4b304a0232128b87'
